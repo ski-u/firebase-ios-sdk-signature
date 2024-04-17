73bd4de0fda4645a3efd440afc89a611b10e10e4
Yunosuke Sakai <qmkns246@gmail.com>
2024-04-17 14:24:22 +0900

    initial commit

---
+++ firebase-ios-sdk-xcframeworks/firebase-ios-sdk-xcframeworks/firebase_ios_sdk_xcframeworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebase_ios_sdk_xcframeworksApp.swift
+//  firebase-ios-sdk-xcframeworks
+//
+//  Created by sakai.yunosuke on 2024/04/17.
+//
+
+import SwiftUI
+
+@main
+struct firebase_ios_sdk_xcframeworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ firebase-ios-sdk/firebase-ios-sdk/firebase_ios_sdkApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebase_ios_sdkApp.swift
+//  firebase-ios-sdk
+//
+//  Created by sakai.yunosuke on 2024/04/17.
+//
+
+import SwiftUI
+
+@main
+struct firebase_ios_sdkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
